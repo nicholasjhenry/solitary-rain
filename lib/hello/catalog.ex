04ba4ca9a5fa37bb4598bbd236dc971bc71b2101
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-07-20 08:52:48 -0400

    code: In-context Relationships
    https://hexdocs.pm/phoenix/1.8.0-rc.4/in_context_relationships.html

--- lib/hello/catalog.ex
+++ lib/hello/catalog.ex
@@ -35,7 +35,7 @@ defmodule Hello.Catalog do
       ** (Ecto.NoResultsError)
 
   """
-  def get_product!(id), do: Repo.get!(Product, id)
+  def get_product!(id), do: Product |> Repo.get!(id) |> Repo.preload(:categories)
 
   @doc """
   Creates a product.
@@ -51,7 +51,7 @@ defmodule Hello.Catalog do
   """
   def create_product(attrs) do
     %Product{}
-    |> Product.changeset(attrs)
+    |> change_product(attrs)
     |> Repo.insert()
   end
 
@@ -69,7 +69,7 @@ defmodule Hello.Catalog do
   """
   def update_product(%Product{} = product, attrs) do
     product
-    |> Product.changeset(attrs)
+    |> change_product(attrs)
     |> Repo.update()
   end
 
@@ -99,7 +99,12 @@ defmodule Hello.Catalog do
 
   """
   def change_product(%Product{} = product, attrs \\ %{}) do
-    Product.changeset(product, attrs)
+    categories = list_categories_by_id(attrs["category_ids"])
+
+    product
+    |> Repo.preload(:categories)
+    |> Product.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:categories, categories)
   end
 
   def inc_page_views(%Product{} = product) do
@@ -204,5 +209,11 @@ defmodule Hello.Catalog do
   """
   def change_category(%Category{} = category, attrs \\ %{}) do
     Category.changeset(category, attrs)
+  end
+
+  def list_categories_by_id(nil), do: []
+
+  def list_categories_by_id(category_ids) do
+    Repo.all(from c in Category, where: c.id in ^category_ids)
   end
 end
--- lib/hello_web/controllers/product_html.ex
+++ lib/hello_web/controllers/product_html.ex
@@ -14,4 +14,15 @@ defmodule HelloWeb.ProductHTML do
   attr :return_to, :string, default: nil
 
   def product_form(assigns)
+
+  def category_opts(changeset) do
+    existing_ids =
+      changeset
+      |> Ecto.Changeset.get_change(:categories, [])
+      |> Enum.map(& &1.data.id)
+
+    for cat <- Hello.Catalog.list_categories() do
+      [key: cat.title, value: cat.id, selected: cat.id in existing_ids]
+    end
+  end
 end
