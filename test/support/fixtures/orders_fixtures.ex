f38048dac61d1159663bfceac293a6ad25535a96
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-07-20 09:19:28 -0400

    cli: mix phx.gen.context Orders LineItem order_line_items \ price:decimal quantity:integer \ order_id:references:orders product_id:references:products --no-scope

--- test/support/fixtures/orders_fixtures.ex
+++ test/support/fixtures/orders_fixtures.ex
@@ -16,4 +16,19 @@ defmodule Hello.OrdersFixtures do
     {:ok, order} = Hello.Orders.create_order(scope, attrs)
     order
   end
+
+  @doc """
+  Generate a line_item.
+  """
+  def line_item_fixture(attrs \\ %{}) do
+    {:ok, line_item} =
+      attrs
+      |> Enum.into(%{
+        price: "120.5",
+        quantity: 42
+      })
+      |> Hello.Orders.create_line_item()
+
+    line_item
+  end
 end
