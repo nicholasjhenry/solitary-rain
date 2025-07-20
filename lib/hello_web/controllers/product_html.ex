2ede6d45ac382b57bed799076805a63ec8d18502
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-07-20 08:34:07 -0400

    cli: mix phx.gen.html Catalog Product products title:string \ description:string price:decimal views:integer
    https://hexdocs.pm/phoenix/1.8.0-rc.4/your_first_context.html#starting-with-generators

---
+++ lib/hello_web/controllers/product_html.ex
@@ -0,0 +1,17 @@
+defmodule HelloWeb.ProductHTML do
+  use HelloWeb, :html
+
+  embed_templates "product_html/*"
+
+  @doc """
+  Renders a product form.
+
+  The form is defined in the template at
+  product_html/product_form.html.heex
+  """
+  attr :changeset, Ecto.Changeset, required: true
+  attr :action, :string, required: true
+  attr :return_to, :string, default: nil
+
+  def product_form(assigns)
+end
